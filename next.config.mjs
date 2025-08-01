605a279e8d0b51f85c84beafdb9c2623269df02c
hesling2025 <mrhesling@163.com>
2025-08-01 20:19:07 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {  
+    return [  
+      {  
+        // 这里的路径可以根据您的需求进行调整  
+        source: '/(.*)',  
+        headers: [  
+          {  
+            key: 'Access-Control-Allow-Origin',  
+            value: '*', // 或者具体的域名  
+          },  
+          {  
+            key: 'Access-Control-Allow-Methods',  
+            value: 'GET, POST, OPTIONS',  
+          },  
+          {  
+            key: 'Access-Control-Allow-Headers',  
+            value: 'Content-Type',  
+          },  
+        ],  
+      },  
+    ]  
+  },
     async rewrites() {
         return [
             {
